d2f7a1bdf105c52d2912f10ee4f552c8995ce326
Rinkal <rinkaladesara.tops@gmail.com>
2025-12-16 15:53:13 +0530

    fgf

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react'
 // export default defineConfig({
 //   plugins: [react()],
 // })
+
 export default defineConfig({
-  base: "/Demoreact/",
-})
+  plugins: [react()],
+  base: "/Demoreact/"
+});
